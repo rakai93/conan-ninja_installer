81b5cc42d9136146343fb6c49c57aa784a9336ca
Christian Fueller <christian.fueller@gmx.de>
2019-10-16 18:37:19 +0200

    Add building from source if no binary is available

--- conanfile.py
+++ conanfile.py
@@ -7,13 +7,24 @@ class NinjaInstallerConan(ConanFile):
     name = "ninja_installer"
     version = "1.8.2"
     license = "TDB"
-    url = "TBD"
+    url = "https://ninja-build.org/"
     settings = "os_build", "arch_build"
     build_policy = "missing"
     description = "Ninja installer. Useful as a build_requires."
 
     @property
-    def ninja_download_url(self):
+    def binary_available(self):
+        return (
+            self.settings.arch_build == "x86_64"
+            and self.settings.os_build in ("Windows", "Linux", "Macos")
+        )
+
+    def build_requirements(self):
+        if not self.binary_available:
+            self.build_requires("python/3.7.1@bananamonster/misc")
+
+    @property
+    def binary_url(self):
         if self.settings.os_build == "Windows":
             suffix = "win"
         else:
@@ -21,14 +32,31 @@ class NinjaInstallerConan(ConanFile):
         return f"https://github.com/ninja-build/ninja/releases/download/v{self.version}/ninja-{suffix}.zip"
 
     @property
-    def ninja_folder(self):
-        return "ninja"
+    def source_url(self):
+        return f"https://github.com/ninja-build/ninja/archive/v{self.version}.zip"
+
+    def source(self):
+        if self.binary_available:
+            self.output.info(f"Downloading Ninja from {self.binary_url}")
+            tools.get(self.binary_url, destination="bin")
+            if self.settings.os_build != "Windows":
+                os.chmod('bin/ninja', 0o775)
+        else:
+            self.output.info(f"Downloading Ninja sources from {self.source_url}")
+            tools.get(self.source_url)
+            os.rename(f"ninja-{self.version}", "ninja")
 
     def build(self):
-        self.output.info(f"Downloading Ninja from {self.ninja_download_url}")
-        tools.get(self.ninja_download_url, destination="bin")
-        if self.settings.os_build != "Windows":
-            os.chmod('bin/ninja', 0o775)
+        if self.binary_available:
+            return
+
+        python_bin = os.path.join(self.deps_cpp_info["python"].bin_paths[0], "python3.7")
+        with tools.chdir("ninja"):
+            self.run("chmod +x src/*.sh")
+            self.run(f"{python_bin} ./configure.py --bootstrap")
+
+        os.mkdir("bin")
+        os.rename("ninja/ninja", "bin/ninja")
 
     def package(self):
         self.copy("bin/*", dst="bin", keep_path=False)
